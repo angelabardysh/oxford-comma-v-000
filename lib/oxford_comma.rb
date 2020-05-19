857ce6d51522a75eb8d38542a809581acad116f3
Angela Bardysh <angelabardysh@gmail.com>
2020-05-19 01:42:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,1 @@
-def oxford_comma(array)
-    if array.length <2
-    array.join
-  elsif array.length == 2
-    return array.join (' and ')
-  else
-    array[-1].insert(0,"and ")
-    array.join(", ")
-  end
-end
+
