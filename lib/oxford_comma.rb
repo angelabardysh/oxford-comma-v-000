c2595d33721529dcc1509446b17ffea494373f2a
Angela Bardysh <angelabardysh@gmail.com>
2020-05-19 01:55:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     return array.join( " and " )
-  else array[-1].insert(0," and ")
+  else array[-1].insert(0,"and ")
     array.join(", ")
   end
 end
